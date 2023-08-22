fa219aa3fd041160a86393757f52de523b98cdde
Erick Eduardo Petrucelli <erickpetru@gmail.com>
2023-08-22 11:45:40 -0300

    feat: game runner with default scene

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
 import "./style.css"
+import { useGame } from "./useGame"
 
-// TODO: Run the game inside the canvas.
+window.addEventListener("DOMContentLoaded", async () => {
+  const canvas = document.querySelector<HTMLCanvasElement>("canvas")
+  if (!canvas) throw new Error("Canvas not found. Please review your HTML.")
+
+  const { run, resize } = await useGame(canvas)
+  window.addEventListener("resize", resize)
+  run()
+})
